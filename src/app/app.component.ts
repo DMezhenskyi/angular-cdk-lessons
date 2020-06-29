9c89c4d351e6c05a3416089083b61192e50e7e34
mezhik91 <mezhik91@gmail.com>
2020-06-29 12:59:23 +0200

    observer lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,36 @@
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { DialogComponent } from './overlay-example/dialog/dialog.component';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Overlay, OverlayPositionBuilder } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
+import { Platform } from '@angular/cdk/platform';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-cdk-lessons';
+  isWideScreen$: Observable<boolean>;
 
   constructor(
     private overly: Overlay,
-    private positionBuilder: OverlayPositionBuilder
+    private positionBuilder: OverlayPositionBuilder,
+    public platform: Platform,
+    private breakpointObserver: BreakpointObserver
   ) {}
+
+  ngOnInit(): void {
+    if (this.breakpointObserver.isMatched('(max-width: 600px)')) {
+      console.info('The screen width is less than 600px');
+    }
+    this.isWideScreen$ = this.breakpointObserver
+      .observe([Breakpoints.HandsetLandscape])
+      .pipe(map(({ matches }) => matches));
+  }
 
   createDialog() {
     const overlayRef = this.overly.create({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { OverlayExampleModule } from './overlay-example/overlay-example.module';
 
@@ -16,6 +17,7 @@ import { OverlayExampleModule } from './overlay-example/overlay-example.module';
     MatToolbarModule,
     OverlayExampleModule,
     MatButtonModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
